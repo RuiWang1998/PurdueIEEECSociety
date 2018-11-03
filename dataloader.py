a9a7f1de20b8935884d97c2e5cc84bb52bcb930f
RuiWang1998 <ruiwang1998@outlook.com>
2018-11-03 15:55:35 -0400

    Updating main

--- dataloader.py
+++ dataloader.py
@@ -17,3 +17,18 @@ new_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.ToPILImage()
     ])
+
+def loadData(train_dir, test_dir, batch_size):
+    train_dataset = torchvision.datasets.ImageFolder(root=(train_dir), 
+                                                     transform=transforms.ToTensor())
+    test_dataset = torchvision.datasets.ImageFolder(root=(test_dir), 
+                                                    transform=transforms.ToTensor())
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=BATCH_SIZE, 
+                                               shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=BATCH_SIZE, 
+                                              shuffle=False)
+
+    return train_loader, test_loader
